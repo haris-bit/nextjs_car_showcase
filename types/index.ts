e9bc57914c0adecb66a4975e0918e4f76ab038b2
haris <harisiftikhar109@gmail.com>
2023-09-27 14:15:50 +0500

    added the static frontend

---
+++ types/index.ts
@@ -0,0 +1,11 @@
+// an interface defines how a specific structure should look like
+// what variables and values should it have
+
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+  title: string;
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit";
+}
